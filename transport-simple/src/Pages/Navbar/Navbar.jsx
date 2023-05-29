85254f06542af567cf0b4690f28f0ff877dabdb3
Sandeep Bhai <mail4sandeep44@gmail.com>
2023-05-29 16:59:35 +0530

    navbar setUp

--- transport-simple/src/Pages/Navbar/Navbar.jsx
+++ transport-simple/src/Pages/Navbar/Navbar.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from "react";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <div>
-        <h3>Navbar</h3>
+    <div id="Navbar">
+      <div id="logo">
+        <img
+          src="https://www.transportsimple.com/redesign/images/logo.png"
+          alt=""
+        />
+      </div>
+      <div id="pages">
+        <div>
+          <h3>Home</h3>
+        </div>
+        <div>
+          <h3>Feature</h3>
+        </div>
+        <div>
+          <h3>Industry</h3>
+        </div>
+        <div>
+          <h3>About</h3>
+        </div>
+        <div>
+          <h3>Contact Us</h3>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
